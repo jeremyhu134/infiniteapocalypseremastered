44f17052bfa2426b5e7e392d380ea12866ede617
jeremyhu134 <52206356+jeremyhu134@users.noreply.github.com>
2023-03-21 07:33:19 -0400

    Update game.js

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ const config = {
         arcade: {
             gravity: { y: 0 },
             enableBody: true,
-            debug: true
+            //debug: true
         }
     },
     scene:[MenuScene,PauseScene,ArenaScene,BuildScene],
